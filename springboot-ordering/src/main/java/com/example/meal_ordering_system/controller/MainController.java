0109c4fd2ce70158dfa3b673f69dc36fc36522b3
李豪 <123456@qq.com>
2022-04-30 10:03:49 +0800

    网上订餐系统框架

---
+++ springboot-ordering/src/main/java/com/example/meal_ordering_system/controller/MainController.java
@@ -0,0 +1,10 @@
+package com.example.meal_ordering_system.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("main")
+public class MainController {
+
+}
